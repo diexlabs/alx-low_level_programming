19c207244f80e304cc3a2f2dca7c7e11b1e8f072
trevordiex <trevordiex@gmail.com>
2022-09-10 10:44:52 +0100

    fixing failing tests

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,12 @@
 
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	int last_digit;
 	last_digit = n % 10;
 
 	printf("Last digit of %d is %d ", n, last_digit);
