bc7e2ea85325cfd0a3df9af8f35bdec87db57887
trevordiex <trevordiex@gmail.com>
2022-09-14 08:55:16 +0100

    fixed all fialing tests

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,11 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = -n;
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+		last = -last;
+	_putchar('0' + last);
+	return (last);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 
 int main(void)
 {
@@ -7,7 +8,7 @@ int main(void)
 
 	print_last_digit(98);
 	print_last_digit(0);
-	r = print_last_digit(-1024);
+	r = print_last_digit(INT_MIN);
 	printf("%d is the remainder", r);
 	_putchar('0' + r);
 	_putchar('\n');
