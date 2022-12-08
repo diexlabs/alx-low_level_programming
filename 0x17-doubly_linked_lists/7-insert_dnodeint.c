0ef6dec49a930b4c451aa6bc75989c86299ea508
trevordiex <trevordiex@gmail.com>
2022-12-08 23:25:02 +0100

    fixing error 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	else if (!h && (idx == 0))
 		return (add_dnodeint(&next, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	count = 0;
 	curr = *h;
