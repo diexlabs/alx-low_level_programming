3e68f81a8fcd6ddce95c5095b33bbe2e4a78207d
trevordiex <trevordiex@gmail.com>
2023-02-27 17:37:24 +0100

    makefile interview question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''returns the perimeter of the island
+described in grid
+'''
+
+def island_perimeter(grid):
+    '''calculates the perimeter of an
+    island in a grid
+    '''
+    width = len(grid[0])
+    height = len(grid)
+    perimeter = 0
+    for y in range(height):
+        for x in range(width):
+            if grid[y][x] == 1:
+                perimeter += 4
+                if y != 0 and grid[y - 1][x] == 1:
+                    perimeter -= 2
+                if x != 0 and grid[y][x - 1] == 1:
+                    perimeter -= 2
+    return perimeter
